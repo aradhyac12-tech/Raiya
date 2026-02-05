e4b941d1458e66d6013fd5dc56557c6eb45d2618
aradhyac12-tech <aradhyachavan2@gmail.com>
2026-02-05 18:32:25 +0600

    Create shayari.js

---
+++ js/shayari.js
@@ -0,0 +1,10 @@
+const t=`na dur na karib hai tu
+par mere dil ke pass hai tu
+kabhi ruthna to kabhi manana hai
+kabhi pyar to kabhi gussa hai
+chod ye bato ko tu
+muze toh tuze chahna hai
+bate band mat kara kr muzse
+tere pyar mai he to fida hai hum`;
+let i=0;
+(function type(){ if(i<t.length){poem.textContent+=t[i++]; setTimeout(type,80);} })();
